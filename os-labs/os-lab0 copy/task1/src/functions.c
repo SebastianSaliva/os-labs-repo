2674505c9fecacd82cde74393933fac484252d86
SebastianSaliva <sebastian.saliva@upr.edu>
2024-08-26 18:15:08 -0400

    initial set up

---
+++ os-labs/os-lab0 copy/task1/src/functions.c
@@ -0,0 +1,5 @@
+#include "functions.h"
+
+#include <stdio.h>
+
+void PrintTest() { printf("Test\n"); }
---
+++ os-labs/os-lab0/task3/include/functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void PrintTest();
+
+#endif  // FUNCTIONS_H
