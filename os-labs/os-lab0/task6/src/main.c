e253bb89882325287ce7b1a2c3d36beeecdb1cf3
SebastianSaliva <sebasagir@gmail.com>
2024-08-30 18:53:39 -0400

    task 5 wip

--- os-labs/os-lab0/task6/src/main.c
+++ os-labs/os-lab0/task6/src/main.c
@@ -1,12 +1,12 @@
 #include "functions.h"
 int main() {
-  int nf = 4;
-  int nc = 20;
+  int lenH = 4;
+  int lenW = 20;
 
   char **text;
-  text = malloc(nf * sizeof(char *));
-  for (int i = 0; i < nf; i++) {
-    text[i] = malloc(nc * sizeof(char));
+  text = malloc(lenH * sizeof(char *));
+  for (int i = 0; i < lenH; i++) {
+    text[i] = malloc(lenW * sizeof(char));
   }
 
   gets(text[0]);
