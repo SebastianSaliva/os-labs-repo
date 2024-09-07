c3ad0c5491cc1022124b3a9ced101b980a5879dd
SebastianSaliva <sebasagir@gmail.com>
2024-09-06 22:19:37 -0400

    task 2

---
+++ os-labs/os-lab-2/task6/build/CMakeFiles/TestName.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestName.dir/src/functions.c.o"
+  "CMakeFiles/TestName.dir/src/functions.c.o.d"
+  "CMakeFiles/TestName.dir/src/main.c.o"
+  "CMakeFiles/TestName.dir/src/main.c.o.d"
+  "TestName"
+  "TestName.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
